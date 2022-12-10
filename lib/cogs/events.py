5a1c98a83320a7647eb209f94ae1afe123b409b8
ItsZabbs <hawazubair@gmail.com>
2022-12-10 09:27:44 +0530

    remove support server url

--- lib/cogs/events.py
+++ lib/cogs/events.py
@@ -38,9 +38,10 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx:Union[commands.Context,discord.Interaction], err:BaseException):
         embed=discord.Embed(title='An error occurred - ',description=err,colour=ctx.me.colour if ctx.me.colour.value else discord.Colour.blurple())
-        value='Ask about this in the [support server](https://discord.gg/FBFTYp7nnq)'
-        value+=f' \n or you can check the [wiki](https://ItsZabbs.github.io/Pokedex-Bot{"#"+x if (x:=ctx.command.extras.get("url","").lower()) else ""})'
-        embed.add_field(name='Still confused?',value=value)
+        value=''
+        value+=f'You can check the [wiki](https://ItsZabbs.github.io/Pokedex-Bot{"#"+x if (x:=ctx.command.extras.get("url","").lower()) else ""})'
+        if value:
+            (embed.add_field(name='Still confused?',value=value))
         if ctx.guild is None:
             await ctx.send(embed=embed)
         try:
