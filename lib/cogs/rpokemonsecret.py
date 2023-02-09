2c8930f43fc2c8703d2c6a197c8dbf61ef137cfa
ItsZabbs <hawazubair@gmail.com>
2023-02-09 09:25:50 +0530

    fix guild id

--- lib/cogs/rpokemonsecret.py
+++ lib/cogs/rpokemonsecret.py
@@ -1,13 +1,13 @@
 from discord.app_commands import command,guilds,Choice
 from lib.bot import rpokemon_guild_id,secret_role_id,Bot
 from lib.cogs.pokemon import move_names
-from discord import Interaction,Role
+from discord import Interaction,Role,Object
 from typing import Optional
 
 role:Optional[Role]=None
 
 @command(name='use',description='Use a move')
-@guilds(rpokemon_guild_id)
+@guilds(Object(rpokemon_guild_id))
 async def use(interaction:Interaction,move:str):
     if move.lower().replace(" ","").strip()=="secretpower":
         await interaction.user.add_roles(roles=[role],reason='Used secret power')
@@ -24,4 +24,4 @@ async def autocomplete(interaction:Interaction,current:str):
 async def setup(bot:Bot):
     bot.tree.add_command(use)
     global role
-    role=bot.get_guild(rpokemon_guild_id).get_role(secret_role_id)
+    role=bot.get_guild(int(rpokemon_guild_id)).get_role(int(secret_role_id))
