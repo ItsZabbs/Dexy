6ca993298477eb2ab0f1a298662b84ac45979c3b
Zabbs <74313798+ItsZabbs@users.noreply.github.com>
2023-04-25 15:47:08 +0530

    Fix cog not loading when starting up

--- lib/cogs/rpokemonsecret.py
+++ lib/cogs/rpokemonsecret.py
@@ -1,17 +1,20 @@
 from discord.app_commands import command,guilds,Choice
 from lib.bot import rpokemon_guild_id,secret_role_id,Bot
 from lib.cogs.pokemon import move_names
-from discord import Interaction,Role,Object
+from discord import Interaction,Role,Object,Member,Role,Guild
 from typing import Optional
 
 role:Optional[Role]=None
+assert isinstance(rpokemon_guild_id,int) and isinstance(secret_role_id,int)
 
 @command(name='use',description='Use a move')
 @guilds(Object(rpokemon_guild_id))
 async def use(interaction:Interaction,move:str):
+    assert isinstance(interaction.user,Member) and isinstance(interaction.guild,Guild)
+    assert role is not None
     if move.lower().replace(" ","").strip().replace("-","")=="secretpower":
         await interaction.user.add_roles(role,reason='Used secret power')
-        await interaction.response.send_message("You used Secret Power! You discovered #secret_base!",ephemeral=True)
+        await interaction.response.send_message(f"You used Secret Power! You discovered {getattr(interaction.guild.get_channel(1071525828933992488),'mention','#secret_base')}!",ephemeral=True)
     else:
         await interaction.response.send_message("There was no effect...",ephemeral=True)
 @use.autocomplete(name='move')
@@ -24,4 +27,6 @@ async def autocomplete(interaction:Interaction,current:str):
 async def setup(bot:Bot):
     bot.tree.add_command(use)
     global role
-    role=bot.get_guild(int(rpokemon_guild_id)).get_role(int(secret_role_id))
+    await bot.wait_until_ready()
+    role=(await bot.fetch_guild(int(rpokemon_guild_id))).get_role(secret_role_id)
+    assert isinstance(role,Role)
