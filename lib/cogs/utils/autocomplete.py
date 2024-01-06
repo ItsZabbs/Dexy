b4f2e3a0f28fc88530ea21e58ec798af367b3a8b
Zabbs <74313798+ItsZabbs@users.noreply.github.com>
2024-01-06 14:14:45 +0530

    Improve autocomplete on the backend

--- lib/cogs/utils/autocomplete.py
+++ lib/cogs/utils/autocomplete.py
@@ -6,17 +6,17 @@ import ujson
 with open(
     "lib/cogs/pokedexdata/pokemon_stuff_english_only.json", encoding="utf-8"
 ) as hmm:
-    pokedex_dict: dict = ujson.load(hmm)
+    pokedex_dict: dict[str,dict[str,str]] = ujson.load(hmm)
     id_dict = {}
     for k, v in pokedex_dict.items():
         if id_dict.get(str(v["num"]), None) is None:
             id_dict[str(v["num"])] = (k, v["name"])
-    pokemon_names_disp = [v["name"] for v in pokedex_dict.values()]
+    pokemon_names_disp = [(v["name"],k) for (k,v) in pokedex_dict.items()]
 def pokemon_autocomplete(func):
     async def inner(interaction:Interaction,current:str):
         return [
-            Choice(name=pokemon, value=pokemon)
-            for pokemon in pokemon_names_disp
+            Choice(name=pokemon, value=pokemondictname)
+            for pokemon,pokemondictname in pokemon_names_disp
             if current.lower() in pokemon.lower()
         ][:25]
     return inner
